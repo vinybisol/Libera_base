eebb29a852072146bce84b08fbfc2aae86d7dfaa
Viny Bisol <vinybisol@hotmail.com>
2020-07-30 13:50:33 -0300

    chupa

---
+++ Libera_base/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Data;
+
+namespace Libera_base
+{
+    public class Cliente
+    {
+
+            public int ClienteID { get; set; }
+            public string Nome { get; set; }
+
+            public DataTable PreencheComboBox()
+            {
+
+                Conexao.Conecta();
+
+                NpgsqlCommand comm = new NpgsqlCommand();
+                comm.CommandText = "Select cli_id, cli_razaosocial from Clientes";
+                comm.Connection = Conexao.Conn;
+                DataTable dt = new DataTable();
+                NpgsqlDataReader dr = comm.ExecuteReader();
+                dt.Load(dr);
+
+                Conexao.Desconecta();
+
+                return dt;
+            }
+
+    }
+    
+}
--- Libera_base/Form1.cs
+++ Libera_base/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
+
 
 namespace Libera_base
 {
@@ -19,15 +21,66 @@ namespace Libera_base
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string sql = "select * from filiais";
 
-            Conexao c = new Conexao();
-            
+            Cliente objCliente = new Cliente();
 
-            var volta = c.conexao(sql);
+            var data = objCliente.PreencheComboBox();
+
+            ListView listView1 = new ListView();
+            listView1.Bounds = new Rectangle(new Point(10, 10), new Size(400, 400));
+            // Set the view to show details.
+            listView1.View = View.Details;
+            // Allow the user to edit item text.
+            listView1.LabelEdit = true;
+            // Allow the user to rearrange columns.
+            listView1.AllowColumnReorder = true;
+            // Display check boxes.
+            listView1.CheckBoxes = true;
+            // Select the item and subitems when selection is made.
+            listView1.FullRowSelect = true;
+            // Display grid lines.
+            listView1.GridLines = true;
+            // Sort the items in the list in ascending order.
+            listView1.Sorting = SortOrder.Ascending;
+            // Create three items and three sets of subitems for each item.
+            ListViewItem item1 = new ListViewItem("banana", 0);
+            // Place a check mark next to the item.
+            item1.Checked = false;
+            item1.SubItems.Add("1");
+            item1.SubItems.Add("2");
+            item1.SubItems.Add("3");
+
+
+            // Create columns for the items and subitems.
+            // Width of -2 indicates auto-size.
+            listView1.Columns.Add("Item Column", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Column 2", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Column 3", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Column 4", -2, HorizontalAlignment.Center);
+            //Add the items to the ListView.
+            listView1.Items.AddRange(new ListViewItem[] { item1});
+
+            // Create two ImageList objects.
+            ImageList imageListSmall = new ImageList();
+            ImageList imageListLarge = new ImageList();
+
+    
+
+            //Assign the ImageList objects to the ListView.
+            listView1.LargeImageList = imageListLarge;
+            listView1.SmallImageList = imageListSmall;
+
+            // Add the ListView to the control collection.
+            this.Controls.Add(listView1);
 
 
 
+
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
     }
