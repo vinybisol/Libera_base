eee51253e9bb4ce397a2c62becb7cb795bb18a52
Viny Bisol <vinybisol@hotmail.com>
2020-08-06 13:50:30 -0300

    Sql e roda a função

--- Libera_base/Cliente.cs
+++ Libera_base/Cliente.cs
@@ -5,30 +5,54 @@ using System.Text;
 using System.Threading.Tasks;
 using Npgsql;
 using System.Data;
+using System.Windows.Forms;
 
 namespace Libera_base
 {
     public class Cliente
     {
 
-            public int ClienteID { get; set; }
-            public string Nome { get; set; }
+        public int ClienteID { get; set; }
+        public string Nome { get; set; }
 
-            public DataTable PreencheComboBox()
-            {
+        public DataTable PreencheComboBox()
+        {
 
             Conexao con = new Conexao();
             con.Conecta();
+            NpgsqlCommand comm = new NpgsqlCommand();
+            comm.CommandText = "Select cli_id, cli_razaosocial from Clientes order by cli_id";
+            comm.Connection = Conexao.Conn;
+            DataTable dt = new DataTable();
+            NpgsqlDataReader dr = comm.ExecuteReader();
+            dt.Load(dr);
+            Conexao.Desconecta();
+            return dt;
+        }
+
+        public Boolean ExecutaComando(string sql)
+        {
+            try
+            {
+                Conexao con = new Conexao();
+                con.Conecta();
                 NpgsqlCommand comm = new NpgsqlCommand();
-                comm.CommandText = "Select cli_id, cli_razaosocial from Clientes order by cli_id";
+                comm.CommandText = sql;
                 comm.Connection = Conexao.Conn;
-                DataTable dt = new DataTable();
-                NpgsqlDataReader dr = comm.ExecuteReader();
-                dt.Load(dr);
+                comm.ExecuteReader();
                 Conexao.Desconecta();
-                return dt;
+                return true;
             }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
+            }
+            
+
+
+        }
 
     }
-    
+
 }
---
+++ Libera_base/Exclusao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libera_base
+{
+    class Exclusao
+    {
+        public Boolean ExclusaoNotasEmitidas()
+        {
+            Cliente cli = new Cliente();
+            cli.ExecutaComando("delete from notafiscalemitida where nte_id = 1;");
+            bool deuCerto = true;
+            return deuCerto;
+        }
+    }
+}
